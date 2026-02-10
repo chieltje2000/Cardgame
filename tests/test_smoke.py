d85f474cfc1d407b789fa13b612ecbc0f4c44c07
Chiel Bouwman <bouwmanchiel@gmail.com>
2026-02-10 19:39:41 +0100

    added test for minimal object creation+ fixed test_import_core

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,7 +1,7 @@
 def test_import_core():
-    import core
+    from core import engine, ui
 
-    _ = (core.engine, core.ui)
+    _ = (engine, ui)
 
 
 def test_public_engine_imports():
@@ -26,3 +26,24 @@ def test_public_ui_imports():
     import core.ui as ui
 
     _ = (ui.UI, ui.UICLI, ui.UI2DEngine)
+
+
+def test_minimal_construction():
+    import core.engine as engine
+    import core.ui as ui
+
+    # engine: construct where it has no required dependencies
+    engine.Action()
+    engine.Event()
+    engine.Actor()
+    engine.Observation()
+    engine.Effect()
+    engine.Card()
+    engine.EffectCard()
+    engine.Zone()
+    engine.Deck()
+    engine.RNG()
+
+    # ui: concrete stubs should be instantiable
+    ui.UICLI()
+    ui.UI2DEngine()
